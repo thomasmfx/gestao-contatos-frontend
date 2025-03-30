d7e5d1372efad05c8214587e78d81e695ca04fab
thomasmfx <thomasmoisesf@gmail.com>
2025-03-30 00:18:26 -0300

    🌟 feat: melhorar controle de input em SearchBar

--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -1,14 +1,25 @@
 import { Search } from 'react-feather';
+import { useState } from 'react';
 
-import Button from '../Button/Button';
 import './SearchBar.css';
+import Button from '../Button/Button';
 
 function SearchBar({ placeholder = '', inputName = 'Input', onSearch }) {
+  const [inputValue, setInputValue] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSearch('', inputValue);
+  }
+
   return (
-    <form className="searchbar-form" onSubmit={onSearch}>
+    <form onSubmit={(e) => handleSubmit(e)} className="searchbar-form">
       <input
+        onChange={(e) => setInputValue(e.target.value)}
         className="searchbar-input"
         placeholder={placeholder}
+        value={inputValue}
+        autoComplete="off"
         name={inputName}
       />
       <Button type={'submit'} isSquare>
