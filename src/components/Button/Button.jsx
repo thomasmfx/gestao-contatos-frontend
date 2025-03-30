345cf9e8afa2aeca7627c99671fc1f19a8f9fd53
thomasmfx <thomasmoisesf@gmail.com>
2025-03-30 17:33:27 -0300

    🌟 feat: adicionar props e adicionar opções de estilo

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,16 @@
 import './Button.css';
 
-function Button({ children, type = 'button', variant = '', isSquare = false }) {
+function Button({
+  children,
+  type = '',
+  variant = '',
+  isSquare = false,
+  onClick,
+}) {
   return (
     <button
-      className={`button ${variant}${isSquare ? 'square' : ''}`}
+      className={`button ${variant} ${isSquare ? 'square' : ''}`}
+      onClick={onClick}
       type={type}
     >
       {children}
