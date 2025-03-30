252b4a7d50b1321808ad12adf27e5b493a4d646f
thomasmfx <thomasmoisesf@gmail.com>
2025-03-30 19:03:43 -0300

    🌟 feat: adicionar funcionalidades para CRUD de clientes

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,54 +1,124 @@
 import './Root.css';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+import AddClienteForm from '../components/AddClienteForm/AddClienteForm';
 import { clienteColumns, contatoColumns } from '../utils/tableColumns';
+import ClienteForm from '../components/ClienteForm/ClienteForm';
 import SearchBar from '../components/SearchBar/Searchbar';
 import Button from '../components/Button/Button';
 import useContatos from '../hooks/useContatos';
 import useClientes from '../hooks/useClientes';
 import Table from '../components/Table/Table';
+import Modal from '../components/Modal/Modal';
 
 function Root() {
-  const { clientesData, getClientes } = useClientes();
-  const { contatosData, getContatos } = useContatos();
+  const {
+    clientesData,
+    setClientes,
+    getCliente,
+    addCliente,
+    updateCliente,
+    deleteCliente,
+  } = useClientes();
+  const { contatosData, setContatos, getContatos } = useContatos();
+  const [selectedCliente, setSelectedCliente] = useState(null);
+  const [selectedClienteContatos, setSelectedClienteContatos] = useState(null);
+  const [isAddingCliente, setIsAddingCliente] = useState(false);
 
   useEffect(() => {
-    getClientes();
-    getContatos();
+    setClientes();
+    setContatos();
   }, []);
 
+  function handleSelectCliente(id) {
+    getCliente(id).then((cliente) => {
+      setSelectedCliente(cliente);
+    });
+    getContatos(id).then((contatos) => {
+      setSelectedClienteContatos(contatos);
+    });
+  }
+
+  function handleAddCliente(data) {
+    addCliente(data).then(() => {
+      setIsAddingCliente(false);
+      setClientes();
+    });
+  }
+
+  function handleUpdateCliente(id, newData) {
+    updateCliente(id, newData).then(() => {
+      setSelectedCliente(null);
+      setClientes();
+      setContatos();
+    });
+  }
+
+  function handleDeleteCliente(id) {
+    deleteCliente(id).then(() => {
+      setSelectedCliente(null);
+      setClientes();
+      setContatos();
+    });
+  }
+
   return (
-    <main className="main">
-      <section className="section">
-        <header className="header">
-          <SearchBar
-            placeholder="Buscar cliente por Nome ou CPF"
-            inputName="buscar-cliente"
-            onSearch={getClientes}
+    <>
+      {selectedCliente && selectedClienteContatos ? (
+        <Modal onClose={() => setSelectedCliente(null)}>
+          <ClienteForm
+            onClose={() => setSelectedCliente(null)}
+            contatos={selectedClienteContatos}
+            onDelete={handleDeleteCliente}
+            onSave={handleUpdateCliente}
+            cliente={selectedCliente}
           />
-          <Button variant="add">
-            <span>Novo cliente</span>
-          </Button>
-        </header>
-        <h2>Lista de clientes</h2>
-        <Table columns={clienteColumns} data={clientesData} />
-      </section>
-      <section className="section">
-        <header className="header">
-          <SearchBar
-            placeholder="Buscar contato por ID Cliente"
-            inputName="buscar-contato"
-            onSearch={getContatos}
+        </Modal>
+      ) : null}
+
+      {isAddingCliente && (
+        <Modal onClose={() => setIsAddingCliente(false)}>
+          <AddClienteForm
+            onClose={() => setIsAddingCliente(false)}
+            onSave={handleAddCliente}
           />
-          <Button variant="add">
-            <span>Novo contato</span>
-          </Button>
-        </header>
-        <h2>Lista de contatos</h2>
-        <Table columns={contatoColumns} data={contatosData} />
-      </section>
-    </main>
+        </Modal>
+      )}
+
+      <main className="main">
+        <section className="section">
+          <header className="header">
+            <SearchBar
+              placeholder="Buscar cliente por Nome ou CPF"
+              onSearch={setClientes}
+            />
+            <Button onClick={() => setIsAddingCliente(true)} variant="add">
+              <span>Novo cliente</span>
+            </Button>
+          </header>
+          <h2>Lista de clientes</h2>
+          <Table
+            onRowClick={handleSelectCliente}
+            columns={clienteColumns}
+            data={clientesData}
+          />
+        </section>
+        <section className="section">
+          <header className="header">
+            <SearchBar
+              placeholder="Buscar contato por ID Cliente"
+              onSearch={setContatos}
+            />
+            <Button variant="add">
+              <span>Novo contato</span>
+            </Button>
+          </header>
+          <h2>Lista de contatos</h2>
+          <Table columns={contatoColumns} data={contatosData} />
+        </section>
+      </main>
+    </>
   );
 }
 
