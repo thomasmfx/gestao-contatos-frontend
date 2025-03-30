caafc8078ba81607138e0aa98acd3145773a67a6
thomasmfx <thomasmoisesf@gmail.com>
2025-03-29 22:30:32 -0300

    🌟 feat: adicionar listagem de clientes e contatos

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,5 +1,53 @@
+import './Root.css';
+
+import { useEffect } from 'react';
+
+import { clienteColumns, contatoColumns } from '../utils/tableColumns';
+import SearchBar from '../components/SearchBar/Searchbar';
+import Button from '../components/Button/Button';
+import useContatos from '../hooks/useContatos';
+import useClientes from '../hooks/useClientes';
+import Table from '../components/Table/Table';
+
 function Root() {
-  return <></>;
+  const { clientesData, getClientes } = useClientes();
+  const { contatosData, getContatos } = useContatos();
+
+  useEffect(() => {
+    getClientes();
+    getContatos();
+  }, []);
+
+  return (
+    <main className="main">
+      <section className="section">
+        <header className="header">
+          <SearchBar
+            placeholder="Buscar cliente por Nome ou CPF"
+            inputName="buscar-cliente"
+          />
+          <Button variant="add">
+            <span>Novo cliente</span>
+          </Button>
+        </header>
+        <h2>Lista de clientes</h2>
+        <Table columns={clienteColumns} data={clientesData} />
+      </section>
+      <section className="section">
+        <header className="header">
+          <SearchBar
+            placeholder="Buscar contato por ID Cliente"
+            inputName="buscar-contato"
+          />
+          <Button variant="add">
+            <span>Novo contato</span>
+          </Button>
+        </header>
+        <h2>Lista de contatos</h2>
+        <Table columns={contatoColumns} data={contatosData} />
+      </section>
+    </main>
+  );
 }
 
 export default Root;
