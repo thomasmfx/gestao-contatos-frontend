6a29314e03351f3e0e53bf00b74d2705f53dd818
thomasmfx <thomasmoisesf@gmail.com.br>
2025-07-08 15:34:32 -0300

    feat: implementa rotas

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 
+import router from './router/router';
 import App from './App.vue';
 
-const app = createApp(App);
-
-app.mount('#app');
+createApp(App).use(router).mount('#app');
